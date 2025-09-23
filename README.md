333d0ad2b4cd2d1d2632525ed5f6d0a27a9e2b40
Joshua <joshuadejesus12345@gmail.com>
2025-09-23 09:17:53 +0800

    added index page and login page

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a simple document uploader. 
+Focused on functionality for now.
+
+Future plan: make a proper frontend.
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express=require('express');
 const app=express();
+const path=require('node:path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/',(req,res)=>{
     res.render('index');
+});
+
+app.get('/login',(req,res)=>{
+    res.render('login');
 });
 
 app.listen(process.env.PORT || 3000,()=>{
