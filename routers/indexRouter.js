c1a6dfa1fc2f6c2e7ab3cd30d6fc271513e04511
Joshua <joshuadejesus12345@gmail.com>
2025-09-29 01:00:05 +0800

    added upload functionality

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const multer  = require('multer')
+const upload = multer({ dest: './public/uploads/' })
 
 router.get('/', (req, res) => {
     res.render('index', { user: req.user });
+});
+
+router.post('/', upload.single('document'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+    // File information is available in req.file
+    console.log(req.file);
+    res.send('File uploaded successfully.');
 });
 
 module.exports = router;
